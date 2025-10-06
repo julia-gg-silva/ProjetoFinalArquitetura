ae3e0ee747a092c3542920527180c6d5bd11851e
JULIA GABRIELI GARCIA DA SILVA <julia_gg_silva@estudante.sesisenai.org.br>
2025-10-06 18:12:44 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/example/ControleAlmoxarifado/ControleAlmoxarifadoApplication.java
@@ -0,0 +1,13 @@
+package com.example.ControleAlmoxarifado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleAlmoxarifadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleAlmoxarifadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ControleAlmoxarifado/ControleAlmoxarifadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ControleAlmoxarifado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleAlmoxarifadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
