0d54c26e78c17a86e5716bde4ee62f0dec1343fb
raquel1s <silva.raquel2903@gmail.com>
2025-10-20 18:39:11 -0300

    adicionando as validações nos dtos

--- src/main/java/com/example/ControleAlmoxarifado/dto/fornecedor/CriacaoFornecedorRequisicaoDTO.java
+++ src/main/java/com/example/ControleAlmoxarifado/dto/fornecedor/CriacaoFornecedorRequisicaoDTO.java
@@ -1,7 +1,11 @@
 package com.example.ControleAlmoxarifado.dto.fornecedor;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record CriacaoFornecedorRequisicaoDTO(
+        @NotBlank(message = "O nome é obrigatório!")
         String nome,
+        @NotBlank(message = "O CNPJ é obrigatório!")
         String cnpj
 ) {
 }
--- src/main/java/com/example/ControleAlmoxarifado/dto/requisicao/CriacaoRequisicaoRequisicaoDTO.java
+++ src/main/java/com/example/ControleAlmoxarifado/dto/requisicao/CriacaoRequisicaoRequisicaoDTO.java
@@ -1,15 +1,22 @@
 package com.example.ControleAlmoxarifado.dto.requisicao;
 
 import com.example.ControleAlmoxarifado.model.Material;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
 import java.util.HashMap;
 
 
 public record CriacaoRequisicaoRequisicaoDTO(
+        @NotBlank(message = "O setor é obrigatório")
         String setor,
+        @NotNull(message = "A data de solicitação é obrigatório")
         LocalDate dataSolicitacao,
+        @NotBlank(message = "O status é orbigatório!")
         String status,
+        @NotEmpty(message = "A lista de materiais não pode ser vazia!")
         HashMap<Integer, Double> materiais
 ) {
 }
